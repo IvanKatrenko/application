41db479e1e494fe33be83fe39e2f2298801d547b
berno_4444 <ivankatrenko1997@gmail.com>
2024-12-25 15:01:05 +0100

    new file and component

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center
+         justify-center bg-gray-900">
+            <Text>Root Layout</Text>
+            <StatusBar style="auto" />
+            <Link href="/profile" style={{ color: "red" }}>go to Profile</Link>
+        </View>
+    );
+};
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,tsx,ts,jsx}',
+    './custom directory**/*.{js,tsx,ts,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
