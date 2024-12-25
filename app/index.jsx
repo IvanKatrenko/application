bdd227d77d59be40a36aa3be3279378f3c455765
berno_4444 <ivankatrenko1997@gmail.com>
2024-12-25 23:57:34 +0100

    feat(new file)

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
+import 'expo-router/entry';
 
 export default function App() {
     return (
