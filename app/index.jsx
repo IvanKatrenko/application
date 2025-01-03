010353f938708000d72ec02928b08baccb7f3285
berno_4444 <ivankatrenko1997@gmail.com>
2025-01-03 15:10:57 +0100

    modified search:index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,45 @@
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
-import 'expo-router/entry';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native-web";
+import { images } from "../constants";
 
 export default function App() {
     return (
-        <View className="flex-1 items-center
-         justify-center bg-gray-900">
-            <Text className="text-2xl text-white font-bold">Root Layout</Text>
-            <StatusBar style="auto" />
-            <Link href="/home" style={{ color: "red" }}>go to
-                Home</Link>
-        </View>
+        <SafeAreaView className="flex-1 items-center
+         justify-center bg-primary h-full">
+            <ScrollView contentContainerStyle={{
+                height: "100%"
+            }}>
+                <View className="flex-1 items-center justify-center 
+                h-full px-4">
+                    <Image
+                        source={images.logo}
+                        resizeMode="contain"
+                        className="w-[100px] h-[84px]"
+                    />
+
+                    <Image
+                        source={images.cards}
+                        className="max-w--[380px] w-full h-[300px]"
+                        resizeMode="contain"
+                    />
+
+                    <View className="relative mt-5">
+                        <Text className="text-3xl text-white font-bold 
+                        text-center">
+                            Discover Endless Possibilities with
+                            Our App
+
+                            <Text classname="text-secondary-200">
+                                Aora
+                            </Text>
+                        </Text>
+                    </View>
+                </View>
+
+            </ScrollView>
+        </SafeAreaView>
     );
 };
-
-
