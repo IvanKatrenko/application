36e0b4c8e6d9f1f3bf8d46204e0031b00dee2723
berno_4444 <ivankatrenko1997@gmail.com>
2025-01-08 21:51:30 +0100

    app/(auth):sign-in.jsx

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ScrollView } from 'react-native-web'
+
+import { images } from '../../constants'
 
 const SignIn = () => {
     return (
-        <View>
-            <Text>SignIn</Text>
-        </View>
+        <SafeAreaView className="flex-1 items-center justify-center">
+            <ScrollView>
+                <View className="w-full justify-center h-full px-4 my-6">
+                    <Image source={images.logo} resizeMode="contain" classname="w-[115px] h-[35px]" />
+
+                    <Text className="text-2xl text-white text-semibold font-psemibold">Log in
+                        to your account</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
