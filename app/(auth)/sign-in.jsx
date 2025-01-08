4a17d162cb82874e75877c2dc4971be09b535105
berno_4444 <ivankatrenko1997@gmail.com>
2025-01-08 23:07:29 +0100

    formfiled sign-in and sign-up(auth)

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ScrollView } from 'react-native-web'
 
 import { images } from '../../constants'
+import FormField from '../../components/FormField'
+
 
 const SignIn = () => {
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+
     return (
         <SafeAreaView className="flex-1 items-center justify-center">
             <ScrollView>
@@ -13,7 +21,26 @@ const SignIn = () => {
                     <Image source={images.logo} resizeMode="contain" classname="w-[115px] h-[35px]" />
 
                     <Text className="text-2xl text-white text-semibold font-psemibold">Log in
-                        to your account</Text>
+                        to your account
+                    </Text>
+
+                    <FormField className="mt-5"
+                        title="Email"
+                        value={form.email}
+                        handleChangeText={(text) =>
+                            setForm({ ...form, email: e })}
+                        otherStyles="mt-7"
+                        keyboardType="email-address"
+                    />
+
+                    <FormField className="mt-5"
+                        title="Password"
+                        value={form.password}
+                        handleChangeText={(text) =>
+                            setForm({ ...form, password: e })}
+                        otherStyles="mt-7"
+                    />
+
                 </View>
             </ScrollView>
         </SafeAreaView>
