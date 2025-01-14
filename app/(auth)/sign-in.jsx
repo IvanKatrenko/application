c09ad0dcd395adc55aa22679b84c44064b3a8fac
berno_4444 <ivankatrenko1997@gmail.com>
2025-01-14 22:04:47 +0100

    app(auth)/sign-in.jsx

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -19,23 +19,23 @@ const SignIn = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
+  // registartion function 
   const submit = async () => {
     if (!form.email || !form.password) {
-      Alert.aler('Error', 'Please enter all the required fields');
+      Alert.alert('Error', 'Please enter all the required fields');
     }
 
-    setIsSubmitting(true);
-
+    setSubmitting(true);
     try {
-      await signIn(form.email, form.password)
-
-      //set it to global state...
+      const result = await CreateUser(form.email, form.password, form.username)
+      setUser(result);
+      setIsLoggedIn(true);
 
       router.replace('/home')
     } catch (error) {
       Alert.alert('Error', error.message);
     } finally {
-      setIsSubmitting(false);
+      setIsSubmitting(false); // set loading state to false
     }
   };
 
